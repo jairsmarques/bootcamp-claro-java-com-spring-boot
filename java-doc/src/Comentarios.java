43a5533292211569d096d6fdd7f85b87b33c06f1
Jair de Sousa <jairsmarques1126@gmail.com>
2024-09-10 16:44:18 -0300

    adicionando os arquivos da aula sobre javadoc

---
+++ java-doc/src/Comentarios.java
@@ -0,0 +1,27 @@
+
+public class Comentarios {
+
+	public static void main(String[] args) {
+		
+		// Olá, eu sou um comentário de uma única linha
+		
+		/* Olá,
+		 * E sou um comentário 
+		 * que posso ser mais detalhado
+		 * quando necessário
+		 */
+		
+
+	}
+	/**
+	 * Estas dua estrelinhas acima
+	 * é para identificar que você
+	 * pretende elaborar um comentário 
+	 * a nível de documentação
+	 * Que incrível !!!
+	 */
+	public void metodo() {
+		
+	}
+
+}
