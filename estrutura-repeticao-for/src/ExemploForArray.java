e5ff0062117cec42cee14a3541359f98a157a046
Jair de Sousa <jairsmarques1126@gmail.com>
2024-09-12 10:22:57 -0300

    adicionando o arquivo da aula sobre estrutura de repetição for com array

---
+++ estrutura-repeticao-for/src/ExemploForArray.java
@@ -0,0 +1,15 @@
+
+public class ExemploForArray {
+
+	public static void main(String[] args) {
+		
+		// em arrays o indice de elemento inicia em ZERO
+		String alunos [] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
+		
+		for (int x = 0; x < alunos.length; x++) {
+			System.out.println("O aluno no índice x = " + x + " é " + alunos[x]);
+		}
+
+	}
+
+}
